322e5ad71e12003a7ce8cf81976bcec094b1ae17
oracle68 <53831343+oracle68@users.noreply.github.com>
2025-05-06 10:33:28 +0200

    Add files via upload

---
+++ src/test/java/com/wsboot/WsbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
